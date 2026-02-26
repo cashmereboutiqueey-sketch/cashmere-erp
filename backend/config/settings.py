c766c39b7e4cab03f13791a3a8e64d9522163d99
Abdelrahman <abdelrahmanneamatallah@gmail.com>
2026-02-26 05:41:49 +0200

    Add Pillow for image support

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
+
+import dj_database_url
+import os
+
 from pathlib import Path
 from decouple import config, Csv
 
@@ -96,12 +101,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+               default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600
+    )
 }
+
 
 # CORS Settings
 CORS_ALLOWED_ORIGINS = [
